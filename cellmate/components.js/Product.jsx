c30705589f183763707fc4202273dfac040746a9
Kurt Maglalang <kbm126@live.com>
2022-05-18 14:21:03 -0700

    improve image scaling

--- cellmate/components.js/Product.jsx
+++ cellmate/components.js/Product.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import Link from 'next/link';
-import { urlFor } from '../lib/client';
+import React from "react";
+import Link from "next/link";
+import { urlFor } from "../lib/client";
 
-const Product = ({product:{name, image, slug, price}}) => {
+const Product = ({ product: { name, image, slug, price } }) => {
   return (
     <div>
       <Link href={`/product/${slug.current}`}>
-        <div className='product-card'>
-          <img src={urlFor(image && image[0])} width={300} height={400}/>
-          <div className='product-name'>{name}</div>
-          <div className='product-price'>$ {price}</div>
+        <div className="product-card">
+          <img
+            src={urlFor(image && image[0])}
+            width={"auto"}
+            height={300}
+            object-fit={"contain"}
+          />
+          <div className="product-name">{name}</div>
+          <div className="product-price">$ {price}</div>
         </div>
       </Link>
-
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
