daff177fd7b5a0029ed8f3245a43043bb63b0d19
Kurt Maglalang <kbm126@live.com>
2022-05-20 15:58:03 -0700

    implement basic authentication

--- cellmate/components.js/Navbar.jsx
+++ cellmate/components.js/Navbar.jsx
@@ -1,48 +1,89 @@
-import React from 'react';
-import Link from 'next/link';
-import { AiOutlineShopping } from 'react-icons/ai';
-import { Cart } from '.';
-import { useStateContext } from '../context/StateContext';
+import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
+import { Cart } from ".";
+import { useStateContext } from "../context/StateContext";
+import { useUser } from "@auth0/nextjs-auth0";
 
 const Navbar = () => {
-  const { 
-    showCart, 
-    setShowCart, 
+  const { user } = useUser();
+  const {
+    showCart,
+    setShowCart,
     setSearchInput,
-    toggleAdvSearch, 
-    setToggleAdvSearch, 
-    totalQuantities, 
+    toggleAdvSearch,
+    setToggleAdvSearch,
+    totalQuantities,
     setAdvSearchName,
     setAdvSearchBrand,
     setAdvSearchMaxPrice,
-    setAdvSearchMinPrice 
+    setAdvSearchMinPrice,
   } = useStateContext();
+
+  const userInfo = () => {
+    if (user) {
+      return (
+        <>
+          <h4>Hello, {user.name}!</h4>
+          <a href="/api/auth/logout">Logout</a>
+        </>
+      );
+    }
+
+    return <a href="/api/auth/login">Login</a>;
+  };
+
   return (
-    <div className='navbar-container'>
-      <p className='logo'>
-        <Link href={'/'}>CellMate</Link>
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href={"/"}>CellMate</Link>
       </p>
-   
-        <label className='search-box'>
-          <input placeholder='Quick Search' type="text" name="name" onChange={(e)=> setSearchInput(e.target.value)} />
-        </label>
-        { toggleAdvSearch && <button className='advance-search' 
-        onClick={() => {setToggleAdvSearch(false)  ,  setAdvSearchName(''), 
-        setAdvSearchBrand(''),
-        setAdvSearchMaxPrice(1000000),
-        setAdvSearchMinPrice(0)}
 
-        } >Return</button>}
+      <label className="search-box">
+        <input
+          placeholder="Quick Search"
+          type="text"
+          name="name"
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+      </label>
+      {toggleAdvSearch && (
+        <button
+          className="advance-search"
+          onClick={() => {
+            setToggleAdvSearch(false),
+              setAdvSearchName(""),
+              setAdvSearchBrand(""),
+              setAdvSearchMaxPrice(1000000),
+              setAdvSearchMinPrice(0);
+          }}
+        >
+          Return
+        </button>
+      )}
 
-        { !toggleAdvSearch && <button className='advance-search' onClick={() => setToggleAdvSearch(true)} >Advance Search</button>}
-        
-      <button type='button' className='cart-icon' onClick={() =>setShowCart(true)}>
-        <AiOutlineShopping/>
-        <span className='cart-item-qty'>{totalQuantities}</span>
+      {!toggleAdvSearch && (
+        <button
+          className="advance-search"
+          onClick={() => setToggleAdvSearch(true)}
+        >
+          Advance Search
+        </button>
+      )}
+
+      {userInfo()}
+
+      <button
+        type="button"
+        className="cart-icon"
+        onClick={() => setShowCart(true)}
+      >
+        <AiOutlineShopping />
+        <span className="cart-item-qty">{totalQuantities}</span>
       </button>
-      { showCart && <Cart/>}
+      {showCart && <Cart />}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
