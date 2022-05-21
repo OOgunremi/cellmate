0681ba8a05f98cc31f2ee64fe727f4348bd0d0ca
Kurt Maglalang <kbm126@live.com>
2022-05-20 22:57:41 -0700

    add user email to stripe data if user is logged in

--- cellmate/pages/api/stripe.js
+++ cellmate/pages/api/stripe.js
@@ -1,38 +1,41 @@
-import Stripe from 'stripe'; 
+import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.NEXT_SECRET_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     try {
       const params = {
-        submit_type: 'pay',
-        mode: 'payment',
-        payment_method_types: ['card'],
-        billing_address_collection: 'auto',
+        submit_type: "pay",
+        mode: "payment",
+        payment_method_types: ["card"],
+        billing_address_collection: "auto",
         shipping_options: [
-          { shipping_rate: 'shr_1Kzh1YFHtIwdR0mh11VciczS' },
-          { shipping_rate: 'shr_1Kzh3dFHtIwdR0mhwYCKGBGq' },
+          { shipping_rate: "shr_1L1D4dD4iAppj0qc0iRH8Hjj" },
+          { shipping_rate: "shr_1L1D52D4iAppj0qc9zeI9QaW" },
         ],
-        line_items: req.body.map((item) => {
+        line_items: req.body.cartItems.map((item) => {
           return {
-            price_data: { 
-              currency: 'cad',
-              product_data: { 
+            price_data: {
+              currency: "cad",
+              product_data: {
                 name: item.name,
               },
               unit_amount: item.price * 100,
             },
             adjustable_quantity: {
-              enabled:true,
+              enabled: true,
               minimum: 1,
             },
-            quantity: item.quantity
-          }
+            quantity: item.quantity,
+          };
         }),
         success_url: `${req.headers.origin}/success`,
         cancel_url: `${req.headers.origin}/canceled`,
-      }
+      };
+
+      // add user email to the stripe data if user is logged in
+      if (req.body.user) params["customer_email"] = req.body.user.email;
 
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
@@ -42,7 +45,7 @@ export default async function handler(req, res) {
       res.status(err.statusCode || 500).json(err.message);
     }
   } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
   }
 }
